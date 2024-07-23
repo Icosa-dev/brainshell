ec69296d4eee62c26a71429230409c7b1f25a3c2
H4drian <leoncotten6@gmail.com>
2024-07-22 20:02:20 -0400

    Updated main.c

--- src/main.c
+++ src/main.c
@@ -6,12 +6,21 @@
 
 char* get_input() {
   static char command_buffer[ARG_MAX];
-  fgets(command_buffer, ARG_MAX, stdin);
+  if (fgets(command_buffer, ARG_MAX, stdin) == NULL) {
+    perror("fgets");
+    exit(1);
+  }
+
+  // remove extra newline character
+  size_t len = strlen(command_buuffer);
+  if (len > 0 && command_buffer[len - 1] == '\n') {
+    command_buffer[len - 1] = '\0';
+  }
   return command_buffer;
 }
 
-int* interpret_code(char* code) {
-  char* data_tape = (char*)malloc(TAPE_SIZE * sizeof(char));
+char* interpret_code(char* code) {
+  static char data_tape[TAPE_SIZE];
   int data_pointer = 0;
   char* code_ptr = code;
   char* loop_start = NULL;
@@ -54,18 +63,25 @@ int* interpret_code(char* code) {
         }
         code_ptr++;
     }
-
+  
+  // make sure the tape is null terminated
+  static char result[TAPE_SIZE + 1];
+  memcpy(result, data_tape, TAPE_SIZE);
+  result[TAPE_SIZE] = '\0';
   return data_tape; 
 }
 
 void run_shell(char* data_tape) {
+  if (strlen(data_tape) > ARG_MAX) {
+    fprintf(stderr, "Command too long\n");
+    return;
+  }
   system(data_tape);
-  free(data_tape);
 }
 
 int main() {
   while (1) {
-    char command_buffer[ARG_MAX] = get_input();
+    char* command_buffer = get_input();
     char* data_tape = interpret_code(command_buffer);
     run_shell(data_tape);
   }
